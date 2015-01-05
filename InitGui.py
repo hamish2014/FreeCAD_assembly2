941459489962d1808d7531b1c90000fddf183986
hamish2014 <antoine.dymond@gmail.com>
2015-01-05 10:48:05 +0200

    workbench icon inserted into InitGui.py in xpm format

--- InitGui.py
+++ InitGui.py
@@ -1,8 +1,5 @@
 
 class Assembly2Workbench (Workbench): 
-    import os
-    from assembly2lib import __dir__
-    Icon = os.path.join( __dir__ , 'workBenchIcon.svg' )
     MenuText = 'Assembly 2'
     def Initialize(self):
         from assembly2lib import __dir__
@@ -10,5 +7,72 @@ class Assembly2Workbench (Workbench):
         commandslist = ['importPart', 'updateImportedPartsCommand', 'addCircularEdgeConstraint', 'addAngleConstraint', 'addPlaneConstraint', 'addAxialConstraint', 
                         'degreesOfFreedomAnimation', 'assembly2SolveConstraints','muxAssembly','addPartsList']
         self.appendToolbar('Assembly 2', commandslist)
+
+    # Icon generated using by converting svg to xpm format using Gimp
+    Icon = '''
+/* XPM */
+static char * workBenchIcon_xpm[] = {
+"32 32 27 1",
+"       c None",
+".      c #000003",
+"+      c #000000",
+"@      c #000009",
+"#      c #00066A",
+"$      c #000BCE",
+"%      c #000004",
+"&      c #000883",
+"*      c #000EFF",
+"=      c #000005",
+"-      c #000564",
+";      c #000BC1",
+">      c #000449",
+",      c #000896",
+"'      c #000006",
+")      c #000442",
+"!      c #00099F",
+"~      c #000112",
+"{      c #00087C",
+"]      c #00010A",
+"^      c #000669",
+"/      c #00011C",
+"(      c #000879",
+"_      c #000EF8",
+":      c #000563",
+"<      c #000DEF",
+"[      c #00011A",
+".+++++++++++++++                ",
+"@#$$$$$$$$$$$$$%                ",
+"@&*************=                ",
+"@&*************=                ",
+"@&*************=                ",
+"@&*************=                ",
+"@&*************=                ",
+"@&*************=                ",
+"@&*************=                ",
+"@&*************=                ",
+"@&*************=                ",
+"@&*************=                ",
+"@&*************=                ",
+"@&*************=                ",
+"@-;;;;;;;;;;;;;%                ",
+".+++++++++++++++                ",
+".==============.................",
+"@>,,,,,,,,,,,,,'=)!!!!!!!!!!!!!~",
+"@{*************]=^*************/",
+"@{*************]=^*************/",
+"@{*************]=^*************/",
+"@{*************]=^*************/",
+"@{*************]=^*************/",
+"@{*************]=^*************/",
+"@{*************]=^*************/",
+"@{*************]=^*************/",
+"@{*************]=^*************/",
+"@{*************]=^*************/",
+"@{*************]=^*************/",
+"@{*************]=^*************/",
+"@(_____________]=:<<<<<<<<<<<<<[",
+"%+++++++++++++++.+++++++++++++++"};
+'''
+
 
 Gui.addWorkbench(Assembly2Workbench())
