8f414385edc3484f93a05297a104c4127f496c1c
hamish2014 <antoine.dymond@gmail.com>
2015-09-25 11:10:41 +0200

    Proxies updated to prevent deletetion of assembly2 objects when active != objects document. Can be disabled in assembly2's preference page.\n"

--- viewProviderProxies.py
+++ viewProviderProxies.py
@@ -3,8 +3,12 @@ from pivy import coin
 import traceback
 
 class ImportedPartViewProviderProxy:
-    def onDelete(self, feature, subelements): # subelements is a tuple of strings
-        viewObject = feature
+    def onDelete(self, viewObject, subelements): # subelements is a tuple of strings
+        parms = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Assembly2")
+        if not parms.GetBool('allowDeletetionFromExternalDocuments', False):
+            if FreeCAD.activeDocument() != viewObject.Object.Document:
+                FreeCAD.Console.PrintMessage("preventing deletetion of %s since active document != %s. Disable behavior in assembly2 preferences.\n" % (viewObject.Object.Label, viewObject.Object.Document.Name) )
+                return False
         obj = viewObject.Object
         doc = obj.Document
         #FreeCAD.Console.PrintMessage('ConstraintObjectViewProviderProxy.onDelete: removing constraints refering to %s (label:%s)\n' % (obj.Name, obj.Label))
@@ -43,3 +47,11 @@ class ConstraintViewProviderProxy:
     def getDefaultDisplayMode(self):
         "'''Return the name of the default display mode. It must be defined in getDisplayModes.'''"
         return "Standard"
+
+    def onDelete(self, viewObject, subelements): # subelements is a tuple of strings
+        parms = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Assembly2")
+        if not parms.GetBool('allowDeletetionFromExternalDocuments', False):
+            if FreeCAD.activeDocument() != viewObject.Object.Document:
+                FreeCAD.Console.PrintMessage("preventing deletetion of %s since active document != %s. Disable behavior in assembly2 preferences.\n" % (viewObject.Object.Label, viewObject.Object.Document.Name) )
+                return False
+        return True
