142da6e3469e6ee122854dd6a896e06d08377d62
BPLRFE <Roland-Frank@Web.de>
2016-12-05 00:04:20 +0100

    Mux only selected parts ...
    Mux only selected parts (Thanks to Turro75).

--- muxAssembly.py
+++ muxAssembly.py
@@ -9,7 +9,8 @@ class Proxy_muxAssemblyObj:
 def muxObjects( doc ):
     'combines all the imported shape object in doc into one shape'
     faces = []
-    for obj in doc.Objects:
+    'for obj in doc.Objects:'
+    for obj in doc.getSelection():
         if 'importPart' in obj.Content:
             debugPrint(3, '  - parsing "%s"' % (obj.Name))
             faces = faces + obj.Shape.Faces
@@ -19,18 +20,19 @@ def muxMapColors( doc, muxedObj):
     'call after muxedObj.Shape =  muxObjects(doc)'
     diffuseColors = []
     faceMap = {}
-    for obj in doc.Objects:
+    'for obj in doc.Objects:'
+    for obj in doc.getSelection():
         if 'importPart' in obj.Content:
             for i, face in enumerate(obj.Shape.Faces):
                 if i < len(obj.ViewObject.DiffuseColor):
                     clr = obj.ViewObject.DiffuseColor[i]
                 else:
                     clr = obj.ViewObject.DiffuseColor[0]
-                faceMap[faceMapKey(face)] = clr    
+                faceMap[faceMapKey(face)] = clr   
     for f in muxedObj.Shape.Faces:
         try:
             key = faceMapKey(f)
-            clr = faceMap[key] 
+            clr = faceMap[key]
             del faceMap[key]
         except KeyError:
             debugPrint(3, 'muxMapColors: waring no faceMap entry for %s - key %s' % (f,faceMapKey(f)))
@@ -41,35 +43,39 @@ def muxMapColors( doc, muxedObj):
 def faceMapKey(face):
     c = sum([ [ v.Point.x, v.Point.y, v.Point.z] for v in face.Vertexes ], [])
     return tuple(c)
-    
+   
 class MuxAssemblyCommand:
     def Activated(self):
         #first check if assembly mux part already existings
-        checkResult = [ obj  for obj in FreeCAD.ActiveDocument.Objects 
+        '''checkResult = [ obj  for obj in FreeCAD.ActiveDocument.Objects
                         if hasattr(obj, 'type') and obj.type == 'muxedAssembly' ]
-        if len(checkResult) == 0:
-            partName = 'muxedAssembly'
-            debugPrint(2, 'creating assembly mux "%s"' % (partName))
-            muxedObj = FreeCAD.ActiveDocument.addObject("Part::FeaturePython",partName)
-            muxedObj.Proxy = Proxy_muxAssemblyObj()
-            muxedObj.ViewObject.Proxy = 0
-            muxedObj.addProperty("App::PropertyString","type")
-            muxedObj.type = 'muxedAssembly'
-            FreeCADGui.ActiveDocument.getObject(muxedObj.Name).Visibility = False
-        else:
+        if len(checkResult) == 0:'''
+        #deleted to force a creation of a new one any time
+        partName = 'muxedAssembly'
+        debugPrint(2, 'creating assembly mux "%s"' % (partName))
+        muxedObj = FreeCAD.ActiveDocument.addObject("Part::FeaturePython",partName)
+        muxedObj.Proxy = Proxy_muxAssemblyObj()
+        muxedObj.ViewObject.Proxy = 0
+        muxedObj.addProperty("App::PropertyString","type")
+        muxedObj.type = 'muxedAssembly'
+        FreeCADGui.ActiveDocument.getObject(muxedObj.Name).Visibility = False
+        '''else:
             muxedObj = checkResult[0]
-            debugPrint(2, 'updating assembly mux "%s"' % (muxedObj.Name))
-        muxedObj.Shape = muxObjects( FreeCAD.ActiveDocument )
-        muxMapColors(FreeCAD.ActiveDocument, muxedObj)
+            debugPrint(2, 'updating assembly mux "%s"' % (muxedObj.Name))'''
+            #deleted to force a creation of a new one any time
+        'muxedObj.Shape = muxObjects( FreeCAD.ActiveDocument )'
+        muxedObj.Shape = muxObjects( FreeCADGui.Selection )
+        muxMapColors(FreeCADGui.Selection, muxedObj)
+        'muxMapColors(FreeCAD.ActiveDocument, muxedObj)'
         FreeCAD.ActiveDocument.recompute()
 
        
-    def GetResources(self): 
-        msg = 'Combine selected parts of assembly into a single object (for example to create a drawing view)'
+    def GetResources(self):
+        msg = 'Combine assembly into a single object ( use to create a drawing of the assembly, and so on...)'
         return {
-            'Pixmap' : ':/assembly2/icons/muxAssembly.svg', 
-            'MenuText': msg, 
+            'Pixmap' : ':/assembly2/icons/muxAssembly.svg',
+            'MenuText': msg,
             'ToolTip': msg
-            } 
+            }
 
 FreeCADGui.addCommand('muxAssembly', MuxAssemblyCommand())
